57b45d3025032043637f6c7c7a64e6a05618aae5
TarikBR <daniel.tarik.23@gmail.com>
2023-04-17 14:34:39 -0300

    17/04/2023

---
+++ 17-04-2023/script.js
@@ -0,0 +1,13 @@
+function fazPegadinha(){
+    alert("Olá");
+    alert("4 Ano de Informática");
+    alert("Estão");
+    alert("bastante");
+    alert("entediados?");
+    alert("em ficar");
+    alert("clicando em");
+    alert("ok");
+    alert("né?");
+    alert("Estou esperando vocês aqui no lab");
+}
+fazPegadinha();
